0b8cb74f6a71ed3076b84bbfddea99f255ad6648
Valchris <d_leblond@live.com>
2015-11-19 23:34:34 -0800

    Files missing from Git

---
+++ Tdd/Services/GameService.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNet.SignalR.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Principal;
+using System.Threading.Tasks;
+using System.Web;
+using Tdd.Models;
+
+namespace Tdd.Services
+{
+    public class GameService : IGameService
+    {
+        private readonly IScaleoutService scaleoutService;
+        private readonly IGameRoundService gameRoundService;
+
+        public GameService(IScaleoutService scaleoutService, IGameRoundService gameRoundService) {
+            this.scaleoutService = scaleoutService;
+            this.gameRoundService = gameRoundService;
+        }
+
+        public async Task<GameRoom> StartGameAsync(HubCallerContext context)
+        {
+            var gameRoom = new GameRoom(context);
+            this.scaleoutService.Subscribe(Persist.GameRoom, gameRoom.Id, context);
+            this.scaleoutService.Store(Persist.GameRoom, gameRoom.Id, gameRoom);
+
+            return gameRoom;
+        }
+
+        public async Task<GameRoom> JoinGameAsync(HubCallerContext context, string roomId)
+        {
+            var gameRoom = await this.scaleoutService.Get(Persist.GameRoom, roomId) as GameRoom;
+
+            if(gameRoom != null)
+            {
+                gameRoom.Players.Add(new Player(context));
+                this.scaleoutService.Subscribe(Persist.GameRoom, gameRoom.Id, context);
+                this.scaleoutService.Store(Persist.GameRoom, gameRoom.Id, gameRoom);
+            }
+
+            return gameRoom;
+        }
+
+        public async Task<GameRoom> IncrementRoundAsync(HubCallerContext context, string roomId)
+        {
+
+            var gameRoom = await this.scaleoutService.Get(Persist.GameRoom, roomId) as GameRoom;
+            foreach(var player in gameRoom.Players)
+            {
+                this.scaleoutService.Subscribe(Persist.GameRound, roomId, player.Context);
+            }
+
+            var success = await this.gameRoundService.ProcessRoundAsync(roomId);
+            if(success)
+            {
+                gameRoom.CurrentRound += 1;
+                gameRoom.CurrentRoundStartTime = DateTime.UtcNow;
+                this.scaleoutService.Store(Persist.GameRoom, gameRoom.Id, gameRoom);
+                return gameRoom;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Tdd/Services/ScaleoutService.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Tdd.Controllers;
+using Tdd.Models;
+
+namespace Tdd.Services
+{
+    public class ScaleoutService : IScaleoutService
+    {
+        private readonly IDictionary<string, List<HubCallerContext>> subscriptions;
+
+        public ScaleoutService()
+        {
+            this.subscriptions = new Dictionary<string, List<HubCallerContext>>();
+        }
+
+        public void Subscribe(Persist type, object id, HubCallerContext connection)
+        {
+            if(!this.subscriptions.ContainsKey(this.GetKey(type, id)))
+            {
+                this.subscriptions.Add(this.GetKey(type, id), new List<HubCallerContext>());
+            }
+            this.subscriptions[this.GetKey(type, id)].Add(connection);
+        }
+
+        public void Store(Persist type, object id, object o)
+        {
+            HttpRuntime.Cache.Add(this.GetKey(type, id), o, null, System.Web.Caching.Cache.NoAbsoluteExpiration, TimeSpan.FromMinutes(60), System.Web.Caching.CacheItemPriority.Normal, null);
+
+            if(subscriptions.ContainsKey(this.GetKey(type, id)))
+            {
+                var context = GlobalHost.ConnectionManager.GetHubContext<GameHub>();
+                foreach(var clientContext in subscriptions[this.GetKey(type, id)])
+                {
+                    context.Clients.Client(clientContext.ConnectionId).propertyUpdated(this.GetKey(type, id), o);
+                }
+            }
+        }
+
+        public async Task<object> Get(Persist type, object id)
+        {
+            return HttpRuntime.Cache.Get(this.GetKey(type, id));
+        }
+
+        public async Task<object> Remove(Persist type, object id)
+        {
+            return HttpRuntime.Cache.Remove(this.GetKey(type, id));
+        }
+
+        private string GetKey(Persist type, object name)
+        {
+            return type.ToString() + "-" + name.ToString();
+        }
+    }
+}
