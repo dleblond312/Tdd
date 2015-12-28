aa9d9a3d61083d811db521e9db2d1b4ffca23df0
Valchris <d_leblond@live.com>
2015-12-28 10:23:22 -0800

    Add missing files

---
+++ Tdd/Client/Directives/gameAreaMobDirective.js
@@ -0,0 +1,17 @@
+﻿
+app.directive('gameAreaMob', ['CONSTANTS', 'gameService', 'roundService', function (CONSTANTS, gameService, roundService) {
+    return {
+        scope: {
+            gameAreaMob: '=',
+        },
+        templateUrl: '/Partial/Directives/GameAreaMob.html',
+        link: function (scope, element, attrs) {
+            scope.$on('propertyUpdated', function (event, model) {
+                scope.style = {
+                    xValue: CONSTANTS.GAME_RATIO * scope.gameAreaMob.currentLocation.x + 'px',
+                    yValue: CONSTANTS.GAME_RATIO * scope.gameAreaMob.currentLocation.y + 'px'
+                }
+            });
+        }
+    }
+}]);
---
+++ Tdd/Client/Services/roundService.js
@@ -0,0 +1,14 @@
+﻿
+app.service('roundService', ['$rootScope', function ($rootScope) {
+    var round = {};
+
+    this.getRound = function () {
+        return round;
+    }
+
+    $rootScope.$on('propertyUpdated', function (event, model) {
+        if (model.type.indexOf('GameRound') === 0) {
+            round = model.value;
+        }
+    });
+}]);
